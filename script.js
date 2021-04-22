eca168ebb608a112d700d59b99e7286fd8480b03
ShegoS <jbarretolarrosa@gmail.com>
2021-04-22 20:23:46 -0300

    reworked js file

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function computerPlay() {
     return (computerPick = "scissors");
   }
 }
+
 function gameStart() {
     let playerPick = prompt(
       "Please pick Rock, Paper or Scissors"
     ).toLowerCase();
-    if (playerPick === "rock" || playerPick === "paper" || playerPick === "scissors") {
+    if (playerPick == "rock" || playerPick == "paper" || playerPick == "scissors") {
       let computer = computerPlay();
-      return (computer && playRound());
+      return (computer && playRound(playerPick, computer));
     } else {
       alert("Please enter a valid answer!");
       return gameStart();
@@ -23,17 +24,18 @@ function gameStart() {
 let game = gameStart();
 
 function playRound(playerPick, computerPick) {
-    if (playerPick === "scissors" && computerPick === "paper") {
+  console.log(playerPick, computerPick);  
+  if (playerPick == "scissors" && computerPick == "paper") {
       alert("Computer picked Paper!\nYou win!");
-    } else if (playerPick === "rock" && computerPick === "paper") {
+    } else if (playerPick == "rock" && computerPick == "paper") {
       alert("Computer picked Paper!.\nYou Lose!");
-    } else if (playerPick === "rock" && computerPick === "scissors") {
+    } else if (playerPick == "rock" && computerPick == "scissors") {
       alert("Computer picked Scissors!\nYou win!");
-    } else if (playerPick === "paper" && computerPick === "rock") {
+    } else if (playerPick == "paper" && computerPick == "rock") {
       alert("Computer picked Rock!\nYou win!");
-    } else if (playerPick === "paper" && computerPick === "scissors") {
+    } else if (playerPick == "paper" && computerPick == "scissors") {
       alert("Computer picked scissors!\nYou lose!");
-    } else if (playerPick === "scissors" && computerPick === "rock") {
+    } else if (playerPick == "scissors" && computerPick == "rock") {
       alert("Computer picked Rock!\nYou lose!");
     } else {
       alert("Computer picked the same!\nIt's a draw!");
@@ -42,3 +44,4 @@ function playRound(playerPick, computerPick) {
 
 
 
+console.log(gameStart());
