b3d9e4a6b99c44c7a9467eb3cf5a87ae7c54f86a
ShegoS <jbarretolarrosa@gmail.com>
2021-04-22 19:28:23 -0300

    worked on js file

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function computerPlay() {
+  let computerPick = Math.random();
+  if (computerPick <= 0.3) {
+    return (computerPick = "rock");
+  } else if (0.3 < computerPick && computerPick <= 0.6) {
+    return (computerPick = "paper");
+  } else {
+    return (computerPick = "scissors");
+  }
+}
+function gameStart() {
+    let playerPick = prompt(
+      "Please pick Rock, Paper or Scissors"
+    ).toLowerCase();
+    if (playerPick === "rock" || playerPick === "paper" || playerPick === "scissors") {
+      let computer = computerPlay();
+      return (computer && playRound());
+    } else {
+      alert("Please enter a valid answer!");
+      return gameStart();
+    }
+}
+let game = gameStart();
+
+function playRound(playerPick, computerPick) {
+    if (playerPick === "scissors" && computerPick === "paper") {
+      alert("Computer picked Paper!\nYou win!");
+    } else if (playerPick === "rock" && computerPick === "paper") {
+      alert("Computer picked Paper!.\nYou Lose!");
+    } else if (playerPick === "rock" && computerPick === "scissors") {
+      alert("Computer picked Scissors!\nYou win!");
+    } else if (playerPick === "paper" && computerPick === "rock") {
+      alert("Computer picked Rock!\nYou win!");
+    } else if (playerPick === "paper" && computerPick === "scissors") {
+      alert("Computer picked scissors!\nYou lose!");
+    } else if (playerPick === "scissors" && computerPick === "rock") {
+      alert("Computer picked Rock!\nYou lose!");
+    } else {
+      alert("Computer picked the same!\nIt's a draw!");
+    }
+  }
 
 
-console.log()
+
